dab1a6877a5c447f0a52b76bc06ae84d02ecb7b9
Zak Richardson <zakrc.richardson@gmail.com>
2016-08-03 12:15:04 +0100

    thermostat has max temperatures

---
+++ spec/featureSpec.js
@@ -0,0 +1,55 @@
+'use strict';
+
+describe('Feature Test:', function(){
+
+  var thermostat;
+
+  beforeEach(function(){
+    thermostat = new Thermostat();
+  });
+
+  it ('has default temperature of 20 degrees', function() {
+    expect(thermostat.temperature).toEqual(20)
+  });
+
+  describe('up button', function(){
+    it ('increases the temperature', function() {
+      thermostat.up();
+      expect(thermostat.temperature).toEqual(21)
+    });
+  });
+
+  describe('down button', function(){
+    it ('decreases the temperature', function() {
+      thermostat.down();
+      expect(thermostat.temperature).toEqual(19)
+    });
+
+    it ('has minimum temperature of 10 degrees', function() {
+      for (var i = 0; i < 10; i++) {
+        thermostat.down();
+      }
+      expect( function() {thermostat.down()} ).toThrowError('cannot go below minimum temperature');
+    });
+  });
+
+  describe('power saving mode on ', function(){
+    it ('has a max temp of 25', function() {
+      thermostat.powerSavingMode('on');
+      for (var i = 0; i < 5; i++) {
+        thermostat.up();
+      }
+      expect( function() {thermostat.up()} ).toThrowError('cannot go above maximum temperature');
+    });
+  });
+
+  describe('power saving mode off ', function(){
+    it ('has a max temp of 32', function() {
+      thermostat.powerSavingMode('off');
+      for (var i = 0; i < 12; i++) {
+        thermostat.up();
+      }
+      expect( function() {thermostat.up()} ).toThrowError('cannot go above maximum temperature');
+    });
+  });
+});
---
+++ src/thermostat.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function Thermostat() {
+  this.temperature = 20;
+  this.MAXIMUM_TEMPERATURE = 32
+  this.POWERSAVING_TEMPERATURE = 25
+}
+
+Thermostat.prototype.up = function() {
+  if ( this.temperature === this.max_temperature ) {
+    throw new Error('cannot go above maximum temperature');
+  }
+  this.temperature++;
+};
+
+Thermostat.prototype.down = function() {
+  if ( this.temperature === 10 ) {
+    throw new Error('cannot go below minimum temperature');
+  }
+  this.temperature--;
+};
+
+Thermostat.prototype.powerSavingMode = function(mode) {
+  if ( mode === 'on' ) {
+    this.max_temperature = this.POWERSAVING_TEMPERATURE;
+  } else {
+    this.max_temperature = this.MAXIMUM_TEMPERATURE;
+  }
+};
